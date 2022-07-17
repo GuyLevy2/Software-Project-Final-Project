dd124e2190bc1e850ddd8b0ab82e2127dc5b94a9
GuyLevy2 <92468631+GuyLevy2@users.noreply.github.com>
2022-07-17 14:56:52 +0300

    First Session

---
+++ src/spkmeans.h
@@ -0,0 +1,5 @@
+int wam_func(double *** vectors_list, int N, int dim, double *** wamMat);
+
+int ddg_func(int N, double *** wamMat, double *** ddgMat);
+
+int lNorm_func(int N, double *** wamMat, double *** ddgMat, double *** lNormMat);
