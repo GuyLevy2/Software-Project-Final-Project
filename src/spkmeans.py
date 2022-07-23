794faf56805d56c5740b4c08c77a40574b6ca89b
liadiluz8 <liadiluz@mail.tau.ac.il>
2022-07-23 23:29:01 +0300

    doucumenting .py file and reorgenizing methons

--- src/spkmeans.py
+++ src/spkmeans.py
@@ -7,7 +7,7 @@ def main():
     try:
         processedArgs = validateAndProcessInput(sys.argv)
         
-        if len(processedArgs) == 0:
+        if len(processedArgs) == 0: # processedArgs = [] in case of invalid input
             print("Invalid Input!")
             return
         
@@ -19,75 +19,129 @@ def main():
         goal = processedArgs[5]
 
         if goal == "spk":
-            if K == 0:  # computes K by the eigengap heuristic
+            if K == 0:  # computes K by the eigengap heuristic method
                 K = mksp.eigengapHeuristic_fit(N, dimension, fileContent)
+                # Error handling
+                if K == None:
+                    raise Exception
             
             # K-means++ algorithem
             returnValue_K_meansPP = k_meansPP(fileContent, N, K, dimension)
+            
+            # Error handling
             if returnValue_K_meansPP == None:
-                print("An Error Has Occurred")
-                return
+                raise Exception
             
             centroids_keys = returnValue_K_meansPP[0]
             centroids_list = returnValue_K_meansPP[1]
-            # OUTPUT - Print
-            print(','.join(str(item) for item in centroids_keys))
             
-            for i in range(K):
-                print(','.join(str("%.4f"%item) for item in centroids_list[i]))
+            # Output - printing
+            print(','.join(str(item) for item in centroids_keys))
+            printFloatMatrix_format(K, centroids_list)
 
         elif goal == "wam":
             wam = mksp.wam_fit(N, dimension, fileContent)
+            
+            # Error handling
             if wam == None:
-                print("An Error Has Occurred")
-                return
-
-            for i in range(N):
-                 print(','.join(str("%.4f"%item) for item in wam[i]))
+                raise Exception
+            
+            # Output - printing
+            printFloatMatrix_format(N, wam)
 
         elif goal == "ddg":
             ddg = mksp.ddg_fit(N, dimension, fileContent)
             if ddg == None:
-                print("An Error Has Occurred")
-                return
+                raise Exception
 
-            for i in range(N):
-                 print(','.join(str("%.4f"%item) for item in ddg[i]))
+            # Output - printing
+            printFloatMatrix_format(N, ddg)
         
         elif goal == "lnorm":
             lnorm = mksp.lnorm_fit(N, dimension, fileContent)
-            if lnorm == None: 
-                print("An Error Has Occurred")
-                return
             
-            for i in range(N):
-                 print(','.join(str("%.4f"%item) for item in lnorm[i]))
+            # Error handling
+            if lnorm == None: 
+                raise Exception
+            
+            # Output - printing
+            printFloatMatrix_format(N, lnorm)
 
         elif goal == "jacobi":
             eigenValues, eigenVectors = mksp.jacobi_fit(N, fileContent)
-            # TODO check if returned value is None
-            # TODO check the printing disign (eigen vectors should be printed as columns)
+            
+            # Error handling
+            if eigenValues == None or eigenVectors == None:
+                raise Exception
 
+            # Output - printing
             print(','.join(str("%.4f"%item) for item in eigenValues))
-            for i in range(N):
-                print(','.join(str("%.4f"%item) for item in eigenVectors[i]))
+            printFloatMatrix_format(N, eigenVectors)
     
-    except Exception:
+    except:
         print("An Error Has Occurred")
         return 
     
     return 
 
-def k_meansPP(vectorsList, N, K, dimension):
-    MAX_ITER = 300
-    EPSILON = 0 # ???????TODO???????
+def printFloatMatrix_format(numOfRows, matrix):
+    """ 
+    Function: printFloatMatrix_format:
+    ----------------------------------
+    prints a matrix with float etries with a `,` between 2 following etries
+        and `\n` between 2 following rows
+
+    Parameters:
+    -----------
+    numOfRows: int
+        The number of rows in matrix
+    matrix: list
+        A 2-dimentional matrix with float etries
     
-    sortedOriginalKyes = vectorsList[:,0]    # The first column of vectors list represanted the original keys ordered
+    Return: None
+    """
+    for i in range(numOfRows):
+        print(','.join(str("%.4f"%item) for item in matrix[i]))
+    
+    return
+
+def k_meansPP(vectorsList, N, K, dimension):
+    """ 
+    Function: k_meansPP:
+    --------------------
+    runs k-means++ algorithm:
+    1. Computes initial k cetroids for k-means algorithm
+    2. Runs k-means algorithm
+
+    Parameters:
+    -----------
+    vectorsList: list
+        A list of N vectors, each of them with float etries
+    N: int
+        The number of vectors in vectorsList
+    K: int
+        The number of initial k-centroids
+    dimention: int
+        The dimention of the vectors
+    
+    Return:
+    -------
+    list
+        a list of 2 lists:
+        1. A list of the initial k centroids indexes
+        2. A list of the final k centroids after k-means running
+    returns None in case of error durnig the function
+    """
+
+    MAX_ITER = 300
+    EPSILON = 0 # ???????TODO NOT MENTIONED???????
+    
+    #sortedOriginalKyes = vectorsList[:,0] TODO not need. there is no key coulmn
 
     np.random.seed(0) # SEED
 
-    centroids_indexes = []
-    centroids_keys = []
+    centroids_indexes = []  # index from 0 to N-1
+    #centroids_keys = [] TODO not need. there is no key coulmn
 
     indexes = range(N)
     D = [0.0 for i in indexes]
@@ -95,25 +149,25 @@ def k_meansPP(vectorsList, N, K, dimension):
     i = 0
     random_centroid_ind = np.random.choice(N)
     centroids_indexes.append(random_centroid_ind)
-    centroids_keys.append((int)(sortedOriginalKyes[random_centroid_ind]))
+    #centroids_keys.append((int)(sortedOriginalKyes[random_centroid_ind])) TODO not need. there is no key coulmn
 
     while(i < K - 1):
         for ell in indexes:
             min_dist = -1
 
-            for j in range(i+1):
-                dist = np.square(np.linalg.norm(vectorsList[ell][1:] - vectorsList[centroids_indexes[j]][1:]))
+            for j in range(i + 1):
+                dist = np.square(np.linalg.norm(vectorsList[ell][:] - vectorsList[centroids_indexes[j]][:])) #TODO not need [1:]. there is no key coulmn
                 if min_dist == -1 or dist < min_dist:
                     min_dist = dist
             D[ell] = min_dist
 
-        P = D / (np.sum(D)) # Probebilities array
+        P = D / (np.sum(D)) # computing Probebilities array
 
         i+=1
         
         random_centroid_ind = np.random.choice(N, p=P)
         centroids_indexes.append(random_centroid_ind)
-        centroids_keys.append((int)(sortedOriginalKyes[random_centroid_ind]))
+        #centroids_keys.append((int)(sortedOriginalKyes[random_centroid_ind]))
 
     # Reordering vectorsList s.t. the first k vectors are the k cetroids have chosen in kmeans++ algorithem
     first_k_vectors = vectorsList[centroids_indexes]
@@ -122,20 +176,46 @@ def k_meansPP(vectorsList, N, K, dimension):
 
     # Cut the key value of vectorsList and convert numpu array into python list
     vectorsList = np.array(vectorsList)
-    vectorsList = vectorsList[:,1:]
+    #vectorsList = vectorsList[:,1:] TODO not need. there is no key coulmn
     vectorsList = vectorsList.tolist()
 
     # run kmeans algorithen throw C-API
     centroids_list = mksp.kmeans_fit(K, dimension, N, MAX_ITER, EPSILON, vectorsList)
     if centroids_list == None:
-        return None # Rise error     
+        return None # Raise error     
     
-    return [centroids_keys, centroids_list]
+    #return [centroids_keys, centroids_list] TODO not need. there is no key coulmn
+    return [centroids_indexes, centroids_list]
+
 
 def validateAndProcessInput(argsList):
+    """ 
+    Function: validateAndProcessInput:
+    ----------------------------------
+    validates the initial arguments of the program
+
+    Parameters:
+    -----------
+    argsList: list
+        A list of the program's arguments
+
+    Return:
+    -------
+    list
+        a list of 2 lists:
+        1. A list of the initial k centroids indexes
+        2. A list of the final k centroids after k-means running
+    returns [] in case of invalid input.
+    The input arguments must be 4 elments:
+    1. The name of the program
+    2. An integer-values parameter k
+    3. The goal of the program from GOALSLIST
+    4. A path to .txt or .csv file contains float-valued vectors
+    """
+
     falseList = [] # will be returned in case of invalidation
-    legalFileSuff = [".txt",".csv"]
-    goalsList = ["spk", "wam", "ddg", "lnorm", "jacobi"]
+    LEGALFILES = [".txt",".csv"]
+    GOALSLIST = ["spk", "wam", "ddg", "lnorm", "jacobi"]
 
     if len(argsList) != 4:
         return falseList
@@ -144,10 +224,10 @@ def validateAndProcessInput(argsList):
     goal = str(argsList[2])
     inputFileName = str(argsList[3])
     
-    if goal not in goalsList:
+    if goal not in GOALSLIST:
         return falseList
 
-    if len(inputFileName) < 5 or inputFileName[-4:] not in legalFileSuff:
+    if len(inputFileName) < 5 or inputFileName[-4:] not in LEGALFILES:
         return falseList
     
     try:
@@ -157,28 +237,16 @@ def validateAndProcessInput(argsList):
         fileContent = pd.read_csv(file, header=None)
         fileContent = fileContent.to_numpy()
         
-        #### Sotring the vectors by the column key ###
+        #### Sotring the vectors by the column key ###          TODO not need
         #fileContent = fileContent[fileContent[:,0].argsort(),:] TODO not need
-        dimension = fileContent.shape[1] - 1
+        #dimension = fileContent.shape[1] - 1                    TODO there is no key coulmn
+        dimension = fileContent.shape[1]
         line_count = fileContent.shape[0]
 
-        if k >= line_count or k < 0: # if K >= N
+        if k >= line_count or k < 0:
             return falseList
-
-
-    except FileNotFoundError:
-        return falseList
-
-    except ValueError:
-        return falseList
-    
-    except IndexError:
-        return falseList
-
-    except NameError:
-        return falseList
-    
-    except Exception:
+  
+    except:
         return falseList
     
     return [k, dimension, line_count, inputFileName, fileContent, goal]
