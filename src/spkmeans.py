f6054593bb18ddfa9b12e0958f29af3a830445a3
GuyLevy2 <92468631+GuyLevy2@users.noreply.github.com>
2022-08-28 16:56:05 +0300

    a

--- src/spkmeans.py
+++ src/spkmeans.py
@@ -26,10 +26,10 @@ def main():
                 raise Exception
             print("c") # TODO - remove - testing
             K = np.asarray(T).shape[1] # The final K in the number of columns of the matrix T
+            print("d") # TODO - remove - testing
             # stage (6): Treating each row of T as a point in RxK, cluster them into k clusters via the K-means algorithm
             returnValue_K_meansPP = k_meansPP(T, N, K, K)            
             
-            print("d") # TODO - remove - testing
             
             # Error handling
             if returnValue_K_meansPP == None:
