2720344a306a5fa3f8ac958152204fd9001b417a
GuyLevy2 <92468631+GuyLevy2@users.noreply.github.com>
2022-08-28 12:01:03 +0300

    again

--- src/spkmeans.h
+++ src/spkmeans.h
@@ -1,20 +1,19 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <ctype.h>
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
 
-int wam_func(double***, int, int, double***);
-int ddg_func(int, double***, double***);
-int lNorm_func(int, double***, double***, double***);
-int jacobi_func(int, double***, double***, double**);
-int kmeans_c(int, int, int, int, double, double**, double***);
+    int wam_func(double***, int, int, double***);
+    int ddg_func(int, double***, double***);
+    int lNorm_func(int, double***, double***, double***);
+    int jacobi_func(int, double***, double***, double**);
+    int kmeans_c(int, int, int, int, double, double**, double***);
 
-double** initMat(int);
-double** initMatMN(int, int);
-void freeMat(int, double***);
+    double** initMat(int);
+    double** initMatMN(int, int);
+    void freeMat(int, double***);
 
-int eigenGap(int, double**);
-int sortEigenValuesAndEigenVectors(int, double**, double***);
-int Fill_K_LargestEigenVectors(int, int, double***, double***);
-int ReNormalizedRows(int, int, double***, double***);
+    int eigenGap(int, double**);
+    int sortEigenValuesAndEigenVectors(int, double**, double***);
+    int Fill_K_LargestEigenVectors(int, int, double***, double***);
+    int ReNormalizedRows(int, int, double***, double***);
+
+#endif
