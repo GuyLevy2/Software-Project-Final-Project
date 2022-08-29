c83a76a2e598680c98a1fecdf487999840e877f6
GuyLevy2 <92468631+GuyLevy2@users.noreply.github.com>
2022-08-29 16:29:59 +0300

    a

--- src/spkmeans.c
+++ src/spkmeans.c
@@ -38,7 +38,7 @@ int main(int argc, char *argv[]) {
          &inputFile, &vectorsList, &goal);
     
     if (!inputError){
-        printf("Invalid Input!#"); /* TODO - delete #*/
+        printf("Invalid Input!");
         return 1;
     }
 
